68c3b3e418f040fa6162a087fbc7f64cedb32f3c
june <sometimesclear@gmail.com>
2022-09-15 00:02:22 +0900

    sync main

---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\ldh\\blog\\gatsby-starter-blog\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\ldh\\blog\\gatsby-starter-blog\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\ldh\\blog\\gatsby-starter-blog\\src\\pages\\index.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("C:\\Users\\ldh\\blog\\gatsby-starter-blog\\src\\pages\\using-typescript.tsx"))
+}
+
